d811a816dc383b656b86d87fbe3c4f2dd1f0fff3
JDerekZ <Jplasencia03@gmail.com>
2022-07-09 00:38:58 -0400

    initial commit

---
+++ ArkNotes/frmArkNotes.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace ArkNotes
+{
+    partial class frmArkNotes
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmArkNotes));
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.rtbTexArea = new System.Windows.Forms.RichTextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.btnFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnNew = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnEdit = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnUndo = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnRedo = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnCopy = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnCut = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnPaste = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.btnSelectAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnDeleteAll = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnStyle = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnColor = new System.Windows.Forms.ToolStripMenuItem();
+            this.btnFont = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.tableLayoutPanel1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 20F));
+            this.tableLayoutPanel1.Controls.Add(this.rtbTexArea, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.menuStrip1, 0, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 2;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 7.558139F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 92.44186F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(547, 317);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // rtbTexArea
+            // 
+            this.rtbTexArea.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.rtbTexArea.Location = new System.Drawing.Point(3, 26);
+            this.rtbTexArea.Name = "rtbTexArea";
+            this.rtbTexArea.Size = new System.Drawing.Size(541, 288);
+            this.rtbTexArea.TabIndex = 0;
+            this.rtbTexArea.Text = "";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnFile,
+            this.btnEdit,
+            this.btnStyle});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(547, 23);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // btnFile
+            // 
+            this.btnFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNew,
+            this.btnOpen,
+            this.btnSave,
+            this.btnExit});
+            this.btnFile.Name = "btnFile";
+            this.btnFile.Size = new System.Drawing.Size(37, 19);
+            this.btnFile.Text = "File";
+            // 
+            // btnNew
+            // 
+            this.btnNew.Name = "btnNew";
+            this.btnNew.Size = new System.Drawing.Size(188, 22);
+            this.btnNew.Text = "New                 Ctrl+N";
+            this.btnNew.Click += new System.EventHandler(this.btnNew_Click);
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(188, 22);
+            this.btnOpen.Text = "Open                Ctrl+O";
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(188, 22);
+            this.btnSave.Text = "Save                  Ctrl+S";
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(188, 22);
+            this.btnExit.Text = "Exit                    Alt+F4";
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // btnEdit
+            // 
+            this.btnEdit.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnUndo,
+            this.btnRedo,
+            this.toolStripSeparator1,
+            this.btnCopy,
+            this.btnCut,
+            this.btnPaste,
+            this.toolStripSeparator2,
+            this.btnSelectAll,
+            this.btnDeleteAll});
+            this.btnEdit.Name = "btnEdit";
+            this.btnEdit.Size = new System.Drawing.Size(39, 19);
+            this.btnEdit.Text = "Edit";
+            // 
+            // btnUndo
+            // 
+            this.btnUndo.Name = "btnUndo";
+            this.btnUndo.Size = new System.Drawing.Size(217, 22);
+            this.btnUndo.Text = "Undo                  Crt+Z";
+            this.btnUndo.Click += new System.EventHandler(this.btnUndo_Click);
+            // 
+            // btnRedo
+            // 
+            this.btnRedo.Name = "btnRedo";
+            this.btnRedo.Size = new System.Drawing.Size(217, 22);
+            this.btnRedo.Text = "Redo                  Ctrl+Shit+Z";
+            this.btnRedo.Click += new System.EventHandler(this.btnRedo_Click);
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(214, 6);
+            // 
+            // btnCopy
+            // 
+            this.btnCopy.Name = "btnCopy";
+            this.btnCopy.Size = new System.Drawing.Size(217, 22);
+            this.btnCopy.Text = "Copy                  Ctrl+C";
+            this.btnCopy.Click += new System.EventHandler(this.btnCopy_Click);
+            // 
+            // btnCut
+            // 
+            this.btnCut.Name = "btnCut";
+            this.btnCut.Size = new System.Drawing.Size(217, 22);
+            this.btnCut.Text = "Cut                     Ctrl+X";
+            this.btnCut.Click += new System.EventHandler(this.btnCut_Click);
+            // 
+            // btnPaste
+            // 
+            this.btnPaste.Name = "btnPaste";
+            this.btnPaste.Size = new System.Drawing.Size(217, 22);
+            this.btnPaste.Text = "Paste                  Ctrl+V";
+            this.btnPaste.Click += new System.EventHandler(this.btnPaste_Click);
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(214, 6);
+            // 
+            // btnSelectAll
+            // 
+            this.btnSelectAll.Name = "btnSelectAll";
+            this.btnSelectAll.Size = new System.Drawing.Size(217, 22);
+            this.btnSelectAll.Text = "Select All             Ctrl+A";
+            this.btnSelectAll.Click += new System.EventHandler(this.btnSelectAll_Click);
+            // 
+            // btnDeleteAll
+            // 
+            this.btnDeleteAll.Name = "btnDeleteAll";
+            this.btnDeleteAll.Size = new System.Drawing.Size(217, 22);
+            this.btnDeleteAll.Text = "Delete All           Ctrl+D";
+            this.btnDeleteAll.Click += new System.EventHandler(this.btnDeleteAll_Click);
+            // 
+            // btnStyle
+            // 
+            this.btnStyle.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnColor,
+            this.btnFont});
+            this.btnStyle.Name = "btnStyle";
+            this.btnStyle.Size = new System.Drawing.Size(44, 19);
+            this.btnStyle.Text = "Style";
+            // 
+            // btnColor
+            // 
+            this.btnColor.Name = "btnColor";
+            this.btnColor.Size = new System.Drawing.Size(180, 22);
+            this.btnColor.Text = "Color";
+            this.btnColor.Click += new System.EventHandler(this.btnColor_Click);
+            // 
+            // btnFont
+            // 
+            this.btnFont.Name = "btnFont";
+            this.btnFont.Size = new System.Drawing.Size(180, 22);
+            this.btnFont.Text = "Font";
+            this.btnFont.Click += new System.EventHandler(this.btnFont_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // frmArkNotes
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 317);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmArkNotes";
+            this.Text = "Ark Notes";
+            this.Load += new System.EventHandler(this.frmArkNotes_Load);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.RichTextBox rtbTexArea;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem btnFile;
+        private System.Windows.Forms.ToolStripMenuItem btnNew;
+        private System.Windows.Forms.ToolStripMenuItem btnOpen;
+        private System.Windows.Forms.ToolStripMenuItem btnEdit;
+        private System.Windows.Forms.ToolStripMenuItem btnStyle;
+        private System.Windows.Forms.ToolStripMenuItem btnSave;
+        private System.Windows.Forms.ToolStripMenuItem btnExit;
+        private System.Windows.Forms.ToolStripMenuItem btnUndo;
+        private System.Windows.Forms.ToolStripMenuItem btnRedo;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem btnCopy;
+        private System.Windows.Forms.ToolStripMenuItem btnCut;
+        private System.Windows.Forms.ToolStripMenuItem btnPaste;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripMenuItem btnSelectAll;
+        private System.Windows.Forms.ToolStripMenuItem btnDeleteAll;
+        private System.Windows.Forms.ToolStripMenuItem btnColor;
+        private System.Windows.Forms.ToolStripMenuItem btnFont;
+    }
+}
+
---
+++ ArkNotes/frmArkNotes.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ArkNotes
+{
+    public partial class frmArkNotes : Form
+    {
+
+        String File;
+
+
+        public frmArkNotes()
+        {
+            InitializeComponent();
+        }
+
+        private void frmArkNotes_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+//File Menu Strip
+        private void btnNew_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Clear();
+            File = null;
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Text|*.text";
+
+            if (ofd.ShowDialog()==DialogResult.OK)
+            {
+                File = ofd.FileName;
+                using(StreamReader read = new StreamReader(File))
+                { 
+                    rtbTexArea.Text = read.ReadToEnd();
+                }
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog SaveFile = new SaveFileDialog();
+            SaveFile.Filter = "Texto|*.txt";
+            if (File != null)
+            {
+                using (StreamWriter G = new StreamWriter(File))
+                {
+                    G.Write(rtbTexArea.Text);
+                }
+            }
+            else
+            {
+                if (SaveFile.ShowDialog() == DialogResult.OK)
+                {
+                    File = SaveFile.FileName;
+                    using (StreamWriter g = new StreamWriter(SaveFile.FileName))
+                    {
+                        g.Write(rtbTexArea.Text);
+
+                    }
+                }
+            }
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+//End File Strip
+
+
+//Edit Menu Strip
+        private void btnUndo_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Undo();
+        }
+
+        private void btnRedo_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Redo();
+        }
+
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Copy();
+        }
+
+        private void btnCut_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Cut();
+        }
+
+        private void btnPaste_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Paste();
+        }
+
+        private void btnSelectAll_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.SelectAll();
+        }
+
+        private void btnDeleteAll_Click(object sender, EventArgs e)
+        {
+            rtbTexArea.Clear();
+        }
+
+        private void btnColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog color = new ColorDialog();
+            if (color.ShowDialog() == DialogResult.OK)
+            {
+                rtbTexArea.ForeColor = color.Color;
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            FontDialog fuente = new FontDialog();
+
+            if (fuente.ShowDialog() == DialogResult.OK)
+            {
+                rtbTexArea.Font = fuente.Font;
+            }
+        }
+        //End Menu Strip
+
+    }
+}
